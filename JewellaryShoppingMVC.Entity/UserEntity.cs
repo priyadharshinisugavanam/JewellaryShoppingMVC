9eb165a68847cee07831e5e0d817d808dbf22e49
priyadharshinisugavanam <priyadharshinisugavanam@gmail.com>
2020-02-19 22:28:26 +0530

    Add project files.

---
+++ JewellaryShoppingMVC.Entity/UserEntity.cs
@@ -0,0 +1,34 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewellaryShoppingMVC.Entity
+{
+    public enum City
+    {
+        Salem,
+        Namakkal,
+        Chennai,
+        Trichy        
+    }
+    public class UserEntity
+    {
+        public string userID { get; set; }
+        public string password { get; set; }
+        public string conformPassword { get; set; }
+        public string mailId { get; set; }
+        public string role { get; set; }
+        public string phoneNumber { get; set; }
+        public City city { get; set; }
+        public UserEntity(string userID, string password, string phoneNumber, string mailId)
+        {
+            this.userID = userID;
+            this.password = password;
+            this.mailId = mailId;
+            this.phoneNumber = phoneNumber;
+
+        }
+    }
+}
---
+++ JewellaryShoppingMVC/Controllers/JewellaryShoppingMVCsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JewellaryShoppingMVC.Controllers
+{
+    public class JewellaryShoppingMVCsController : Controller
+    {
+        // GET: JewellaryShoppingMVC
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
